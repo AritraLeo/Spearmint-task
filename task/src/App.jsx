65c12a0270f4d0e4d4d223215455e2fe7d75af31
Aritra Banerjee <103491935+AritraLeo@users.noreply.github.com>
2024-04-26 18:36:29 +0530

    removed /auto

--- task/src/App.jsx
+++ task/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto'; // Import Chart.js
+import Chart from 'chart.js'; // Import Chart.js
 
 function App() {
   const chartRef = useRef(null);
